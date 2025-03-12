8928ca59bbfc858c0aa73d0726ec32524d3133bf
abhinavVP <abhinav.pattanshetti@gmail.com>
2025-03-12 09:30:18 +0530

    Updated hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
-  cout << "this is pes2ug22cs018's lab work\n";
+  cout << "this is pes2ug22cs018's lab 8\n";
   cout << "I have successfully built and run this  \n";
   return 0;
 }
